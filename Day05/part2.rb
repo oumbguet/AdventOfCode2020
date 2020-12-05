813a61339b6ae8492d3a8240bb8c3e88c697ba61
Florian <florian.millet@epitech.eu>
2020-12-05 11:11:28 +0100

    [+] Day05 part2

---
+++ Day05/part2.rb
@@ -0,0 +1,46 @@
+file = File.open("input")
+arr = file.readlines.map(&:chomp)
+ids = []
+
+def get_row(line)
+    pow = 6
+    a, b = 0, 127
+    line.split("").each {|c|
+        if (c == "F")
+            b -= 2 ** pow
+            pow -= 1
+        elsif (c == "B")
+            a += 2 ** pow
+            pow -= 1
+        end
+    }
+    return a
+end
+
+def get_col(line)
+    pow = 2
+    a, b = 0, 8
+    line.split("").each {|c|
+        if (c == "L")
+            b -= 2 ** pow
+            pow -= 1
+        elsif (c == "R")
+            a += 2 ** pow
+            pow -= 1
+        end
+    }
+    return a
+end
+
+arr.each {|line|
+    row = get_row(line[0...-3])
+    col = get_col(line[-3..-1])
+    id = row * 8 + col
+    ids.push(id)
+}
+
+for i in 12...858
+    if ids.count(i) == 0
+        puts i
+    end
+end
